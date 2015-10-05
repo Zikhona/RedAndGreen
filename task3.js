05011d000b260d5b036335895c3d737dc447d24c
Zikhona <zikhonalaurettapoto35@gmail.com>
2015-10-05 15:48:35 +0200

    task3

---
+++ task3.js
@@ -0,0 +1,16 @@
+
+var redbutton = document.getElementById("redbutton");
+redbutton.addEventListener("click", function () {
+var redblock = document.getElementById("block");
+block.className = "red";
+block.innerHTML = "stop";
+});
+
+
+
+var greenbutton = document.getElementById("greenbutton");
+greenbutton.addEventListener("click", function () {
+var greenbutton= document.getElementById("greenbutton");
+block.className = "green";
+block.innerHTML = "go";
+});
