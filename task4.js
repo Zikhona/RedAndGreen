ed27358a3b00d9bf91982d8fadb99c5866ac33dc
Zikhona <zikhonalaurettapoto35@gmail.com>
2015-10-19 11:04:14 +0200

    fix merge conflict

--- task4.js
+++ task4.js
@@ -1,5 +1,4 @@
 var myButton = document.getElementById('button');
-<<<<<<< HEAD
 	var count = 0;
 	var myBlock = document.getElementById('block');
 
@@ -14,37 +13,15 @@ var myButton = document.getElementById('button');
 			break;
 
 		case 2 :
-=======
-var count = 0;
-var myBlock = document.getElementById('block');
-
-myButton.addEventListener('click', function() {
-	count += 1;
-		switch(count) {
-			case 1 :
-			myBlock.className = "grey";
-			myBlock.innerHTML = "Ouch!";
-			myButton.innerHTML = "make red";
-			break;
-
-			case 2 :
->>>>>>> master
 			myBlock.className = "red";
 			myBlock.innerHTML = "stop";
 			myButton.innerHTML = "Make green";
 			break;
 
-<<<<<<< HEAD
 		case 3 :
 			myBlock.className = "green";
 			myBlock.innerHTML = "go";
 			myButton.innerHTML = "Touch me"
-=======
-			case 3 :
-			myBlock.className = "green";
-			myBlock.innerHTML = "go";
-			myButton.innerHTML = "Touch me";
->>>>>>> master
 			count = 0;
 			break;
 		}		
